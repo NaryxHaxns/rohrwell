6035245d83fa9dcc4469a356bee3b79d6a3dfc16
Ryan Nash <nashkbry@gmail.com>
2020-12-17 18:18:12 -0800

    establishing repository

---
+++ src/pages/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  /*--- State ---*/
+  /*--- Handle Methods ---*/
+  /*--- Lifecycle Methods ---*/
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+        <Switch>
+          
+        </Switch>
+      </div>
+    );
+  };
+}
+
+export default App;

91900387d53eee0cf68410147dc0fae156a0c258
Ryan Nash <nashkbry@gmail.com>
2020-12-28 18:19:29 -0800

    edits

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -16,19 +16,21 @@ const AboutPage = () => {
                     <h2>Jeff Rohr</h2>
                     <div className='jeffBioTxt'>
                         <div className='bioPtOneContainer'>
-                            <p className='bioPtOne'>Jeffrey Rohr is a Certified Health Coach and Personal Trainer through the American Council on Exercise. He fell in love with the capabilities of the human body in his teenage years. Back then it started simply by throwing around some dumbbells in the gym. Recognizing the body’s response caused his curiosity to to turn into passion.</p>
+                            <p>Jeffrey Rohr is a Certified Health Coach and Personal Trainer through the American Council on Exercise. He fell in love with the capabilities of the human body in his teenage years. Back then, it started simply by throwing around some dumbbells in the gym. Recognizing the body’s response caused his curiosity to to turn into passion.</p>
                         </div>
                         <div className='jeffQuoteContainer'>
                             <p className='jeffQuote'>“The best part about being human is that we heal.” -Jeff Rohr</p>
                         </div>
                         <div className='jeffBioPtTwoContainer'>
-                            <p className='jeffBioPtTwo'>Jeff became certified in 2013. Since then he has owned and operated his business in helping impact change in others. He also carries certifications in Nutrition, Sr. Adult Fitness, and Sports Medicine. As for himself, Jeff prefers a variation of training styles. From traditional weight lifting to high intensity training, he finds value in it all. He also finds significance in the mind-body connection. Practices such as yoga and other alternative methods help to make coaching an all encompassing experience.</p>
+                            <p>Jeff became certified in 2013. Since then he has owned and operated his business of helping impact change in others. He also carries certifications in Nutrition, Sr. Adult Fitness, and Sports Medicine.</p>
+                            <p>As for himself, Jeff prefers a variation of training styles. From traditional weight lifting to high intensity training, he finds value in it all. He also finds significance in the mind-body connection. Practices such as yoga and other alternative methods help to make coaching an all encompassing experience.</p>
                         </div>
                         <div className='jeffQuoteContainer'>
                             <p className='jeffQuote'>“Ultimately it comes down to a goal oriented design.” -Jeff Rohr</p>
                         </div>
                         <div className='jeffBioPtThreeContainer'>
-                            <p className='jeffBioPtThree'>Jeff has run marathons, gauntlet courses, enjoys hiking, good conversation, and long walks on the... wait, wrong bio... Anyway, if you’re looking for some one to walk boldly with you in the direction you’ve been wanting to go, here’s a teammate.</p>
+                            <p>Jeff has run marathons, gauntlet courses, enjoys hiking, good conversation, and long walks on the... wait, wrong bio...</p>
+                            <p>Anyway, if you’re looking for some one to walk boldly with you in the direction you’ve been wanting to go, here’s a teammate.</p>
                         </div>
                     </div>
                 </div>
